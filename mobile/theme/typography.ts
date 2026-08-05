90312f816ba12103b63f06d5c555e6973e710c1c
Gokhan Alakus <gokhan.alakus71@email.com>
2026-08-05 10:17:07 +0200

    5

---
+++ mobile/theme/typography.ts
@@ -0,0 +1,12 @@
+export const Typography = {
+  title: 28,
+  heading: 22,
+  subheading: 18,
+  body: 16,
+  caption: 14,
+  small: 12,
+
+  weightRegular: "400",
+  weightMedium: "500",
+  weightBold: "700",
+} as const;
