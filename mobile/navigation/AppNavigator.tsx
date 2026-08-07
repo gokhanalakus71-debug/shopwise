39a28fbd8460068321500b6027d25a3b43413b89
Gokhan Alakus <gokhan.alakus71@email.com>
2026-08-07 22:41:54 +0200

    77

--- mobile/navigation/AppNavigator.tsx
+++ mobile/navigation/AppNavigator.tsx
@@ -8,12 +8,14 @@ import WelcomeScreen from "../screens/WelcomeScreen";
 import LoginScreen from "../screens/LoginScreen";
 import HomeScreen from "../screens/HomeScreen";
 import ReviewProductScreen from "../screens/ReviewProductScreen";
+import AddProfileScreen from "../screens/AddProfileScreen";
 
 export type RootStackParamList = {
   Welcome: undefined;
   Login: undefined;
   Home: undefined;
   ReviewProduct: undefined;
+  AddProfile: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -27,12 +29,29 @@ export default function AppNavigator() {
           headerShown: false,
         }}
       >
-        <Stack.Screen name="Welcome" component={WelcomeScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+        />
+
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+        />
+
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+        />
+
         <Stack.Screen
           name="ReviewProduct"
           component={ReviewProductScreen}
+        />
+
+        <Stack.Screen
+          name="AddProfile"
+          component={AddProfileScreen}
         />
       </Stack.Navigator>
     </NavigationContainer>
