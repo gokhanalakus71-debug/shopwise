4dda04e72975337e9d4a1ac71dd498f9c586ee19
Gokhan Alakus <gokhan.alakus71@email.com>
2026-08-14 14:22:32 +0200

    123

---
+++ mobile/services/firebase/firebaseConfig.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBNCZFVhfzbY27lT_DMLRXTwZk0TfI7dg0",
+  authDomain: "shopwise-6763c.firebaseapp.com",
+  projectId: "shopwise-6763c",
+  storageBucket: "shopwise-6763c.firebasestorage.app",
+  messagingSenderId: "264305801598",
+  appId: "1:264305801598:web:4ed42d442cd3e5ecb75b2f",
+};
+
+export const firebaseApp = initializeApp(firebaseConfig);
