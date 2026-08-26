71ec0004083877fa7b126ff7643481f91ce0d2dd
Gokhan Alakus <gokhan.alakus71@email.com>
2026-08-26 16:44:51 +0200

    142

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,13 @@
+import { useEffect } from "react";
 import AppNavigator from "./navigation/AppNavigator";
+import { initializeSubscriptions } from "./services/subscription/subscriptionService";
 
 export default function App() {
+  useEffect(() => {
+    initializeSubscriptions().catch((error) => {
+      console.error("RevenueCat initialization failed:", error);
+    });
+  }, []);
+
   return <AppNavigator />;
 }
